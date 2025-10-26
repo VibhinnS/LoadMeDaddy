4fbe3062b777623bcf893240bb351268bdc7f0f7
Vibhinn Singhal <vibhinn.singhal@iongroup.com>
2025-10-26 18:11:32 +0530

    Switched from threading and socket model to asyncio - major perf and CPU improvement

--- Balancer.py
+++ Balancer.py
@@ -1,54 +1,48 @@
-import socket
-import threading
+import itertools
+import asyncio
 
 from lockpy import private
 
 from config.ConfigReader import ConfigReader
 
 class LoadBalancer:
-    CURRENT_SERVER = 0
-    LOCK = threading.Lock()
-
     def __init__(self):
         self.config_reader = ConfigReader()
         self.configured_servers_ip = self.config_reader.get_config("SERVERS", "IP_ADDRESSES")
         self.configured_server_ports = self.config_reader.get_config("SERVERS", "PORTS")
         self.configured_servers = list(zip(self.configured_servers_ip, self.configured_server_ports))
+        self.configured_server_iterator = itertools.cycle(self.configured_servers)
         self.balancer_ip = self.config_reader.get_config("BALANCER", "IP_ADDRESS")
         self.balancer_port = self.config_reader.get_config("BALANCER", "PORT")
 
-    def handle_incoming_request(self, client_socket: socket.socket):
-        available_server_ip, available_server_port = self.get_available_server()
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.connect((available_server_ip, available_server_port))
+    async def handle_incoming_request(self, client_reader, client_writer):
+        available_server_ip, available_server_port = next(self.configured_server_iterator)
+        server_reader, server_writer = await asyncio.open_connection(available_server_ip, available_server_port)
 
-        threading.Thread(target=self.forward_requests, args=(client_socket, server_socket), daemon=True).start()
-        threading.Thread(target=self.forward_requests, args=(server_socket, client_socket), daemon=True).start()
+        await asyncio.gather(
+            self.forward_requests(client_reader, server_writer),
+            self.forward_requests(server_reader, client_writer)
+        )
 
-    def start_balancer(self):
-        balancer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        balancer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        balancer_socket.bind((self.balancer_ip, self.balancer_port))
-        balancer_socket.listen(100)
+    async def start_balancer(self):
+        load_balancer_server = await asyncio.start_server(
+            self.handle_incoming_request,
+            self.balancer_ip,
+            self.balancer_port
+        )
 
-        while True:
-            client_socket, address = balancer_socket.accept()
-            threading.Thread(target=self.handle_incoming_request, args=(client_socket,), daemon=True).start()
-
-    @private
-    def get_available_server(self):
-        with self.LOCK:
-            server = self.configured_servers[self.CURRENT_SERVER]
-            self.CURRENT_SERVER = (self.CURRENT_SERVER + 1) % len(self.configured_servers)
-        return server
+        async with load_balancer_server:
+            await load_balancer_server.serve_forever()
 
     @private
-    def forward_requests(self, source, destination):
+    async def forward_requests(self, source, destination):
         try:
             while True:
-                data = source.recv(4096)
-                if data: destination.sendall(data)
+                data = await source.read(4096)
+                if not data: break
+                destination.write(data)
+                await destination.drain()
         finally:
-            source.close()
             destination.close()
+            await destination.wait_closed()
 
